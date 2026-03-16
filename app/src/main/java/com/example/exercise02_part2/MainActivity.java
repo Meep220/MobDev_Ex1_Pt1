151e8d386370915ed930d43c2c7dee65fb58990f
Meep220 <cipssly@gmail.com>
2026-03-17 00:30:33 +0800

    Add partial Login Logic

--- app/src/main/java/com/example/exercise02_part2/MainActivity.java
+++ app/src/main/java/com/example/exercise02_part2/MainActivity.java
@@ -18,11 +18,21 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         // EdgeToEdge.enable(this);
         setContentView(binding.getRoot());
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
         binding.Submit.setOnClickListener(v -> validateLogin());
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
+
+    private void validateLogin(){
+        String StudID = binding.SchoolId.getText().toString();
+        String Pass = binding.Password.getText().toString();
+
+
+
+    }
+
 }
