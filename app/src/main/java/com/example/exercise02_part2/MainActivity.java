351f3e53fe85984de4ca05ed2df90739c7e63680
Meep220 <cipssly@gmail.com>
2026-03-17 00:22:04 +0800

    Added ViewBinding Feature

--- app/src/main/java/com/example/exercise02_part2/MainActivity.java
+++ app/src/main/java/com/example/exercise02_part2/MainActivity.java
@@ -1,14 +1,16 @@
 package com.example.exercise02_part2;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.exercise02_part2.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         // EdgeToEdge.enable(this);
         setContentView(binding.getRoot());
+        binding.Submit.setOnClickListener(v -> validateLogin());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
