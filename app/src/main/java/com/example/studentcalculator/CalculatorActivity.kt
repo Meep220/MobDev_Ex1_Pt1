07562086b954e992bb47fe5f6d0a69196c7f8cd8
Meep220 <cipssly@gmail.com>
2026-04-22 22:22:35 +0800

    Feat: Add Login Activity Logic

--- app/src/main/java/com/example/studentcalculator/CalculatorActivity.kt
+++ app/src/main/java/com/example/studentcalculator/CalculatorActivity.kt
@@ -11,10 +11,5 @@ class CalculatorActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_calculator)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
--- app/src/main/java/com/example/studentcalculator/LoginActivity.kt
+++ app/src/main/java/com/example/studentcalculator/LoginActivity.kt
@@ -1,20 +1,44 @@
 package com.example.studentcalculator
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var etStudentId : EditText ;
+    private lateinit var btnLogin : Button ;
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        etStudentId = findViewById<EditText>(R.id.etStudentId)
+        btnLogin = findViewById<Button>(R.id.btnLogin)
+            btnLogin.setOnClickListener {
+                val studentId = etStudentId.text.toString()
+                if (studentId.isEmpty()){
+                    Toast.makeText(this,"Please enter your Student ID",Toast.LENGTH_SHORT).show()
+                } else {
+                    val intent = Intent(this, CalculatorActivity::class.java).apply {
+                        putExtra("STUDENT_ID",studentId)
+                    }
+                    startActivity(intent)
+                    finish()
+
+                }
+
+
+            }
+
     }
 }
